3504fc904a76bc4e9c4797560915e820aa2607ed
Andrew <ramettaandrewj@gmail.com>
2021-03-17 20:48:10 -0400

    modified registration

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -45,16 +45,16 @@ class Register extends Component {
             </div>
             <div>
               <label htmlFor="confirm-password">Confirm Password </label>
-              <p>
-                Password must be atleast 8 characters and include a number, a
-                capitol letter, and a special character
-              </p>
               <input
                 type="password"
                 name="confirmPassword"
                 id="confirm-password"
               />
             </div>
+            <p>
+              Password must be atleast 8 characters and include a number, a
+              capitol letter, and a special character
+            </p>
             <button type="submit">Register</button>
           </form>
         </section>
