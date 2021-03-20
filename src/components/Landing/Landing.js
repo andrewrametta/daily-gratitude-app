6b9de5acbb8034664a2997b2666dee3b628c0494
Andrew <ramettaandrewj@gmail.com>
2021-03-20 19:18:23 -0400

    added images

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -12,33 +12,54 @@ class Landing extends Component {
           of optimism, and it holds for the next six months." -- Shawn Achor
         </p>
         <div className="landing-container">
-          <section className="section-child">
-            <h3 className="card">Change your outlook</h3>
-            <p>
-              We are in some of the most difficult times in modern history.
-              People are suffering from anxiety and depression at higher levels
-              than ever documented before.
-            </p>
-          </section>
-          <section className="section-child">
-            <h3 className="card">How does it work?</h3>
+          <div className="landing-item">
+            <img
+              className="landing-img"
+              src="/mountain.jpg"
+              alt="Daily Gratitude"
+            />
+            <section className="landing-section">
+              <h3 className="card">Change your outlook</h3>
+              <p>
+                We are in some of the most difficult times in modern history.
+                People are suffering from anxiety and depression at higher
+                levels than ever documented before.
+              </p>
+            </section>
+          </div>
+          <div className="landing-item">
+            <img
+              className="landing-img"
+              src="/gothis.jpg"
+              alt="Daily Gratitude"
+            />
+            <section className="landing-section">
+              <h3 className="card">How does it work?</h3>
 
-            <p>
-              You can sign up and create a profile and then start a your own
-              gratitude journal. Each day you can create a new entry. There is
-              also a dashboard page to view your entire journal.
-            </p>
-          </section>
-          <section className="section-child">
-            <h3 className="card">Want to test it out?</h3>
+              <p>
+                You can sign up and create a profile and then start a your own
+                gratitude journal. Each day you can create a new entry. There is
+                also a dashboard page to view your entire journal.
+              </p>
+            </section>
+          </div>
+          <div className="landing-item">
+            <img
+              className="landing-img"
+              src="/yoga.jpg"
+              alt="Daily Gratitude"
+            />
+            <section className="landing-section">
+              <h3 className="card">Want to test it out?</h3>
 
-            <p>
-              We have set up a demo account so that you can test the app. Click
-              the Demo link below and you will land on the login page. The
-              username and password will be auto filled for you, just click
-              login and enjoy!
-            </p>
-          </section>
+              <p>
+                We have set up a demo account so that you can test the app.
+                Click the Demo link below and you will land on the login page.
+                The username and password will be auto filled for you, just
+                click login and enjoy!
+              </p>
+            </section>
+          </div>
         </div>
         <div className="button-container">
           <Link to="/login">
